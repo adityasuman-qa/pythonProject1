d30dacb903bad1c2f6159f753d0962298365513c
ADDY <aksme003@gmail.com>
2024-08-16 23:52:18 +0530

    All file for 14th aug are added with both task modified

--- src/ex_14082024/Lab005.py
+++ src/ex_14082024/Lab005.py
@@ -6,10 +6,10 @@ pow_result = num1** num2
 subst_result = num1-num2
 mul_result = num1*num2
 div_result = num1/num2
-max_result = num1, num2
+max_result = max (num1, num2)
 # Formatting the output using f-string
 print(f"The sum of {num1} and {num2} is {sum_result}.")
-print(f"The pow of {num1} and {num2} is {pow_result}.")
+print(f"The pow of {num1} of {num2} is {pow_result}.")
 print(f"The subst of {num1} and {num2} is {subst_result}.")
 print(f"The mul of {num1} and {num2} is {mul_result}.")
 print(f"The div of {num1} and {num2} is {div_result}.")
