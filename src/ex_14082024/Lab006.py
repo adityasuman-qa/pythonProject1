a520f5742909fe40584da2da09fda77a3e082efd
ADDY <aksme003@gmail.com>
2024-08-16 20:20:55 +0530

    All file for 14th aug are added with both task

--- src/ex_14082024/Lab006.py
+++ src/ex_14082024/Lab006.py
@@ -1,16 +1,10 @@
-# Taking inputs from the user
-num1 = int(input("Enter the first number:"))
-num2 = int(input("Enter the second number:"))
-sum_result = num1 + num2
-pow_result = num1** num2
-subst_result = num1-num2
-mul_result = num1*num2
-div_result = num1/num2
-max_result = num1, num2
-# Formatting the output using f-string
-print(f"The sum of {num1} and {num2} is {sum_result}.")
-print(f"The pow of {num1} and {num2} is {pow_result}.")
-print(f"The subst of {num1} and {num2} is {subst_result}.")
-print(f"The mul of {num1} and {num2} is {mul_result}.")
-print(f"The div of {num1} and {num2} is {div_result}.")
-print(f"The max of {num1} and {num2} is {max_result}.")
+import keyword
+
+print("hello", "Aditya", sep='-', end='_')
+print("Mayra")
+print(keyword.kwlist)
+
+age = 65
+print(age)
+age = "Aditya"
+print(age)
