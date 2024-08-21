98873f40d3ee8fd957b88a2408ef3388001ee0ec
ADDY <aksme003@gmail.com>
2024-08-21 22:33:42 +0530

    All three Task for 20August 2024 are updated here, please verified it.

--- src/ex_20082024/Lab015.py
+++ src/ex_20082024/Lab015.py
@@ -1,9 +1,9 @@
 year = int(input("Enter a year\n"))
-if year/4 and year == 0:
+if year / 4 and year == 0:
     print("Year is a leap year")
-elif year/100 and year == 0:
+elif year / 100 and year == 0:
     print("Year is a leap year")
-elif year/400 and year == 0:
+elif year / 400 and year == 0:
     print("Year is a leap year")
 else:
     print("Year is not a leap year")
